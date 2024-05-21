785e00ebf97651050891c136d25342f828abcc5e
vanshita00 <129608607+vanshita00@users.noreply.github.com>
2024-05-21 17:34:03 +0530

    Add files via upload

---
+++ spring-boot-starter-test/src/test/java/de/vanshita/blog/SpringBootIntegrationTestsWiremockApplicationTests.java
@@ -0,0 +1,13 @@
+package de.vanshita.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootIntegrationTestsWiremockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
